d7d783860372f83f0a1b05e99e1a31b2c63717c2
aymen <aymenadvance@gmail.com>
2017-07-31 14:56:33 +0200

    fix and update

--- javascript.js
+++ javascript.js
@@ -2,13 +2,12 @@ function clickMe(abc){
 	var v=document.getElementsByClassName(abc);
 	hidden('.progress');
    for(var i=0;i<v.length;i=i+1){
-	  v[i].style.visibility = 'visible';
+	  v[i].style.display = 'block';
 	}
 }
 function hidden(elemnt){
    var hide=document.querySelectorAll(elemnt);
    for(var i=0;i<hide.length;i=i+1){
-	  hide[i].style.visibility = 'hidden';
+	  hide[i].style.display = 'none';
 	}
 }
-
