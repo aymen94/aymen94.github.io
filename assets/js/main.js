1fe41b78b352fdd62ac47bfbf436f857cb4491a7
Aymen Naghmouchi <aymen94@users.noreply.github.com>
2018-01-21 13:21:48 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,17 @@
- showHide=id=>{
-	console.log(id);
-	var idToClass=document.getElementsByClassName(id)[0];
-		hidden('.me');
-	 idToClass.style.display='block';
+"use strict";
+function showHide(id) {
+    var idToClass = document.getElementsByClassName(id)[0];
+    hidden('.me');
+    idToClass.style.display = 'block';
 }
-
-clickMe=abc=>{
-	var v=document.getElementsByClassName(abc);
-	hidden('.progress');
-   for(var i=0;i<v.length;i=i+1){
-	  v[i].style.display = 'block';
-	}
+function clickMe(abc) {
+    var v = document.getElementsByClassName(abc);
+    hidden('.progress');
+    for (var i = 0; i < v.length; i++)
+        v[i].style.display = 'block';
 }
-
-hidden=elemnt=>{
-   var hide=document.querySelectorAll(elemnt);
-   for(var i=0;i<hide.length;i=i+1){
-	  hide[i].style.display = 'none';
-	}
+function hidden(elemnt) {
+    var hide = document.querySelectorAll(elemnt);
+    for (var i = 0; i < hide.length; i++)
+        hide[i].style.display = 'none';
 }
