2a2b9a62efe74ec6e13705594b6202ee437fc402
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-08-27 20:41:22 +0200

    add responsive navbar
    Signed-off-by: Aymen Naghmouchi <aymenadvance@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,23 @@
 var change = document.querySelectorAll('H2.hide');
+var menu = document.getElementById('menu');
+var ulMenu = document.querySelector('nav>ul');
 var i = 0;
 
 
-function switchText(){
+function switchText() {
     change[i].classList.remove('show');
-    i = i%(change.length-1);
+    i = i % (change.length - 1);
     change[++i].classList.add('show');
 }
 
-setInterval(switchText,1500);
+setInterval(switchText, 1500);
+
+menu.addEventListener('click', function () {
+    if (ulMenu.className === 'show')
+        ulMenu.classList.remove('show');
+    else
+        ulMenu.classList.add('show');
+});
 
 document.querySelectorAll('a[href^="#"]').forEach(a => {
     a.addEventListener('click', function (e) {
