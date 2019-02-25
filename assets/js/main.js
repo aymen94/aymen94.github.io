13a93220a2f4450fd2779d9ac55faa36a1178470
Aymen Naghmouchi <aymen.aymen@live.it>
2019-02-25 10:44:39 +0100

    update and fix info

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,51 +5,45 @@ var link = document.querySelectorAll('a[href^="#"]');
 var up = document.getElementById('up');
 var i = 0;
 
-
 function switchText() {
-    change[i].classList.remove('show');
-    i = i % (change.length - 1);
-    change[++i].classList.add('show');
+  change[i].classList.remove('show');
+  i = i % (change.length - 1);
+  change[++i].classList.add('show');
 }
 
+window.addEventListener('scroll', function() {
+  this.pageYOffset > 80
+    ? (up.style.display = 'block')
+    : (up.style.display = 'none');
+});
 
-window.addEventListener('scroll', function () {
-    if (this.pageYOffset > 80)
-        up.style.display = 'block';
-    else
-        up.style.display = 'none';
-
-})
-
-menu.addEventListener('click', function () {
-    if (ulMenu.className === 'show')
-        ulMenu.classList.remove('show');
-    else
-        ulMenu.classList.add('show');
+menu.addEventListener('click', function() {
+  if (ulMenu.className === 'show') ulMenu.classList.remove('show');
+  else ulMenu.classList.add('show');
 });
 
 link.forEach(a => {
-    a.addEventListener('click', function (e) {
-        e.preventDefault();
+  a.addEventListener('click', function(e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
 
-window.addEventListener('load', function () {
-    if ('serviceWorker' in navigator)
-        try {
-            navigator.serviceWorker.register('sw.min.js', { scope: './' }).then(function (registration) {
-                console.log('Service worker registration success');
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    else
-        console.log('no serviceWorker');
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator)
+    try {
+      navigator.serviceWorker
+        .register('sw.min.js', { scope: './' })
+        .then(function(registration) {
+          console.log('Service worker registration success');
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  else console.log('no serviceWorker');
 
-    setInterval(switchText, 1500);
-
+  setInterval(switchText, 1500);
 });
