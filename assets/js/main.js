fb48460d8331fba24c10dfbcd21fb5076f26a091
Aymen Naghmouchi <aymenadvance@gmail.com>
2019-06-03 11:46:57 +0200

    remove

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,23 @@ var ulMenu = document.querySelector('nav>ul');
 var up = document.getElementById('up');
 var i = 0;
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   this.pageYOffset > 80
     ? (up.style.display = 'block')
     : (up.style.display = 'none');
 });
 
-document.getElementById('menu').addEventListener('click', function(e) {
+document.getElementById('menu').addEventListener('click', function (e) {
   if (ulMenu.className === 'show') ulMenu.classList.remove('show');
   else ulMenu.classList.add('show');
 });
 
-ulMenu.addEventListener('click', function() {
+ulMenu.addEventListener('click', function () {
   ulMenu.classList.remove('show');
 });
 
 document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener('click', function(e) {
+  a.addEventListener('click', function (e) {
     e.preventDefault();
 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -35,12 +35,12 @@ function switchText() {
   change[++i].classList.add('show');
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   if ('serviceWorker' in navigator)
     try {
       navigator.serviceWorker
         .register('sw.min.js', { scope: './' })
-        .then(function(registration) {
+        .then(function (registration) {
           console.log('Service worker registration success');
         });
     } catch (err) {
