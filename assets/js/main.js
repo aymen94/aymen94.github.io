2c5d6ea9487390419d7415f5f74ffbf9beeb9beb
Aymen Naghmouchi <aymenadvance@gmail.com>
2023-05-17 17:17:04 +0200

    add change color btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener('load', function () {
-  if('serviceWorker' in navigator)
+  if ('serviceWorker' in navigator)
     try {
       navigator.serviceWorker
         .register('sw.min.js', { scope: './' })
@@ -13,3 +13,25 @@ window.addEventListener('load', function () {
     }
   else console.log('no serviceWorker');
 });
+
+window.document.getElementById('changeColorBtn').addEventListener('click', function (et) {
+  let icons = document.getElementsByClassName('icon');
+  if (document.body.style.backgroundColor == 'white') {
+    document.body.style.backgroundColor = 'black';
+    document.getElementById('home').style.backgroundColor = 'white';
+    document.getElementById('home').style.color = 'black';
+    changeColor(icons, 'black');
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.getElementById('home').style.backgroundColor = 'black';
+    document.getElementById('home').style.color = 'white';
+    changeColor(icons, 'white');
+  }
+});
+
+function changeColor(elements, color) {
+  console.log(elements.length);
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].style.fill = color;
+  }
+}
