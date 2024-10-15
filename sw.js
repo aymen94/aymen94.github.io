2064687573ba3025d014aa4d3f11ddf2107e10b8
Aymen Naghmouchi <Aymen.Naghmouchi@acer.com>
2024-10-15 17:05:08 +0200

    update cache

--- sw.js
+++ sw.js
@@ -1,54 +1,45 @@
-const cacheName = "aymenn.xyz-v1";
+const cacheName = "aymenn.xyz-v1.1";
 const assets = [
-	'./',
-	'./index.html',
-	'./assets/css/style.min.css',
-	'./assets/js/main.min.js',
-	'./favicon.ico'
+    './',
+    './index.html',
+    './assets/css/style.min.css',
+    './assets/js/main.min.js',
+    './favicon.ico'
 ];
 
-self.addEventListener('activate', event => {
-	event.waitUntil(
-		caches.keys().then(keyList => {
-			return Promise.all(keyList.map(key => {
-				if (key !== cacheName) {
-					return caches.delete(key);
-				}
-			}));
-		})
-	);
-	event.waitUntil(self.clients.claim());
+self.addEventListener('activate', async event => {
+    event.waitUntil((async () => {
+        const keyList = await caches.keys();
+        await Promise.all(keyList.map(key => {
+            if (key !== cacheName) {
+                return caches.delete(key);
+            }
+        }));
+        await self.clients.claim();
+    })());
 });
 
 self.addEventListener('install', event => {
-	event.waitUntil(
-		caches.open(cacheName)
-			.then(cache => cache.addAll(assets))
-			.then(() => self.skipWaiting())
-	);
+    event.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        await cache.addAll(assets);
+        await self.skipWaiting();
+    })());
 });
 
 self.addEventListener('fetch', event => {
-	if (event.request.url.startsWith('http') || event.request.url.startsWith('https')) {
-		event.respondWith(
-			caches.match(event.request)
-				.then(cachedResponse => {
-					if (cachedResponse) {
-						// Serve from cache  
-						return cachedResponse;
-					}
-					// Perform network request and cache the response  
-					return fetch(event.request).then(fetchRes => {
-						return caches.open(cacheName).then(cache => {
-							cache.put(event.request.url, fetchRes.clone());
-							return fetchRes;
-						});
-					}).catch(() => {
-						// Network request failed, try to serve from cache  
-						return caches.match(event.request);
-					});
-				})
-		);
-	}
+    if (event.request.url.startsWith('http') || event.request.url.startsWith('https')) {
+        event.respondWith((async () => {
+            const cachedResponse = await caches.match(event.request);
+            if (cachedResponse) {
+                // Serve from cache
+                return cachedResponse;
+            }
+            // Perform network request and cache the response
+            const fetchRes = await fetch(event.request);
+            const cache = await caches.open(cacheName);
+            cache.put(event.request, fetchRes.clone());
+            return fetchRes;
+        })());
+    }
 });
-
